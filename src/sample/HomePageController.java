438e3144ee6d5a2edee3ac3bb73f0c8b59fa07ef
Eduarda Trindade <31355194+egtrindade@users.noreply.github.com>
2018-11-29 19:57:59 -0200

    Added Manager Menu Scene (not finished)

--- src/sample/HomePageController.java
+++ src/sample/HomePageController.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class HomePageController {
+
+    public void changeScreenManagerPressed(ActionEvent event) throws Exception{
+            Parent managerMenuParent = FXMLLoader.load(getClass().getResource("/sample/managerMenu.fxml"));
+            Scene managerMenuScene = new Scene(managerMenuParent);
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.setScene(managerMenuScene);
+            window.show();
+    }
 }
--- src/sample/ManagerMenuController.java
+++ src/sample/ManagerMenuController.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class ManagerMenuController {
+    public void changeScreenHomePagePressed(ActionEvent event) throws Exception{
+        Parent homePageParent = FXMLLoader.load(getClass().getResource("/sample/homePage.fxml"));
+        Scene homePageScene = new Scene(homePageParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(homePageScene);
+        window.show();
+    }
 }
