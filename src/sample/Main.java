4dfead24f2aa1662e9cde8df49da7ea5b7f8b654
Eduarda Trindade <31355194+egtrindade@users.noreply.github.com>
2018-11-30 22:36:19 -0200

    Added application Leaf Icon

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,12 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        Image image = new Image("img/leaf.png"); //Icon made by Roundicons from www.flaticon.com
+        primaryStage.getIcons().add(image);
+
         Parent root = FXMLLoader.load(getClass().getResource("homePage.fxml"));
         primaryStage.setTitle("Smart Planting");
         primaryStage.setScene(new Scene(root, 960, 640));
